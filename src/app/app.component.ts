65b54b42340f4cd97860dd1badb13aab38ee56d5
astiksarathe <92151027+astiksarathe@users.noreply.github.com>
2021-10-26 09:36:42 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-assignment 1';
+  firstName:string = 'Astik';
+  lastName:string = 'Sarathe';
+}
